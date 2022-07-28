2ce09b387abb891c2ff24eeca003682615fecd26
Dipayan Banik <dipayan158@gmail.com>
2022-07-28 20:48:11 +0530

    Added all comp

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
       <Services />
       <Portfolio />
       <Testimonials />
+      <Contact />
+      <Footer />
     </>
   );
 };
