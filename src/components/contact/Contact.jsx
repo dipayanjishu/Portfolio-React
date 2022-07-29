0df7a1ac5e2f74ac4434d09d4dbfdfef25068cee
Dipayan Banik <dipayan158@gmail.com>
2022-07-29 22:47:39 +0530

    contact done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,79 @@
 import React from "react";
 import "./Contact.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>dipayan158@gmail.com</h5>
+            <a
+              href="mailto:dipayan158@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a massage
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messanger</h4>
+            <h5>Dipayan Banik</h5>
+            <a
+              href="https://m.me/dipayan.banik.921"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a massage
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+91 8479890195</h5>
+            <a
+              href="https://wa.me/+918479890195"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a massage
+            </a>
+          </article>
+        </div>
+        {/* end of contact
+         */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
