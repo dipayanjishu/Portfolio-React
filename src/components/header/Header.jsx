da90a4985c7592e71f3fe91d71349f502bcdafe3
Dipayan Banik <dipayan158@gmail.com>
2022-07-29 13:02:45 +0530

    photo done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
         <div className="me">
-          <img src={ME} alt="me" />
+          <img src={ME} alt="me" className="laura" />
         </div>
         <a href="#contact" className="scroll__down">
           Scroll Down
